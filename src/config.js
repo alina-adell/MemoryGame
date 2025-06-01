e9da724d8bab975bc7513ef1eb0ca80e9ae82a71
alina-adell <lyhaalina@gmail.com>
2025-06-01 18:07:28 +0200

    first commit

---
+++ src/config.js
@@ -0,0 +1,46 @@
+const config = {
+    cards: [
+        {
+            id: 1,
+            name: "bill-cipher",
+            images: "bill-cipher.svg"
+        },
+        {
+            id: 2,
+            name: "brutus",
+            images: "brutus.svg"
+        },
+        {
+            id: 3,
+            name: "cheburashka",
+            images: "cheburashka.svg"
+        },
+        {
+            id: 4,
+            name: "genie",
+            images: "genie.svg"
+        },
+        {
+            id: 5,
+            name: "mermaid",
+            images: "mermaid.svg"
+        },
+        {
+            id: 6,
+            name: "morty-smith",
+            images: "morty-smith.svg"
+        },
+        {
+            id: 7,
+            name: "ninja-turtle",
+            images: "ninja-turtle.svg"
+        },
+        {
+            id: 8,
+            name: "popeye",
+            images: "popeye.svg"
+        }
+    ]
+};
+
+export default config;
